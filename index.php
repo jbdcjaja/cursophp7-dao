057573a2fd232777fc34206c605fa5468e0096f0
jbdcjaja <jbdc_jaja@yahoo.com.br>
2019-09-04 15:42:01 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -8,10 +8,25 @@ require_once("config.php");
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 //echo json_encode($usuarios);
+//traz um usuário
+//$root->loadbyId(3);
+//echo "cheguei";
+//echo $root;
+//traz uma lista de usuarios
 
-$root = new Usuario();
-$root->loadbyId(3);
-echo "cheguei";
-echo $root;
+
+//Carrega uma lista de usuarios
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+// carrega uma lista dos usuários buscar pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+//carrega o usuário carregando login e a senha
+$usuario = new Usuario();
+$usuario->login("root","9876");
+
+echo $usuario
 
 ?>
